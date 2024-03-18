336ac54719bfaa4625352111cf3ce0df7687b519
lina-yasmine <hadline29@gmail.com>
2024-03-18 04:10:23 +0100

    ts + sém + quad + ASM

--- mot.h
+++ mot.h
@@ -7,7 +7,6 @@ typedef struct
     char val[100];
 } element;
 
-
 typedef struct
 {
     int state;
@@ -15,20 +14,12 @@ typedef struct
     char type[20];
 } elt;
 
-
 element tab[1000];
 elt tabs[40], tabm[40];
 
-static char chaine [] = "";
-
+static char chaine[] = "";
 
 void initialisationMC();
-void insererMC (char entite[], char code[], char type[], float val, int i, int y);
-void rechercherMC (char entite[], char code[], char type[], float val,int y);
+void insererMC(char entite[], char code[], char type[], float val, int i, int y);
+void rechercherMC(char entite[], char code[], char type[], float val, int y);
 void afficherMS();
-
-
-
-
-
-
--- quad.h
+++ quad.h
@@ -1,11 +1,11 @@
 #pragma once
-#include"ts.h"
+#include "ts.h"
 struct quad liste[2000];
 int qc = 0;
 
-void generer(char* a, char* b, char* c, char* d);
-void quadL(int i, char*b, char* c, char* d);
-char* convert(int i);
-void quadC(int i, char* b, char* c, char* d);
+void generer(char *a, char *b, char *c, char *d);
+void quadL(int i, char *b, char *c, char *d);
+char *convert(int i);
+void quadC(int i, char *b, char *c, char *d);
 void afficher_qdr();
 void delete_quad();
